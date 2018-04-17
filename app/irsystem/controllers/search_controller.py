a1ddda37befeca8462231d33476729fd480d82fa
LeeChrison <chrislee@dhcp-rhodes-2503.eduroam.cornell.edu>
2018-04-17 14:23:04 -0400

    add result page

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -18,9 +18,13 @@ def search():
 	  	#query['start_altitude'] = [request.form['start_altitude_lb'], request.form['start_altitude_ub']]
 	  	#print(query)
 	  	ranking = handle_query(query, default, reviews)
-		return render_template('search.html',  data=ranking[:3])
+		return redirect(url_for('irsystem.result', r1=ranking[0]))
 	else:
 		return render_template('search.html')
 
 
 
+@irsystem.route('irsystem.result', methods=["GET","POST"])
+def result():
+	r1 = request.args.get('r1')
+	return 'This is the result page. Trail ID is: ' + str(r1)
