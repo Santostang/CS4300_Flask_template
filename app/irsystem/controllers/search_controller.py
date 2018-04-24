ad554b565efbb747314d6305e441a721de6a40dd
LeeChrison <chrislee@dhcp-rhodes-3019.eduroam.cornell.edu>
2018-04-23 22:19:07 -0400

    trail name & state filter working

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,7 +1,8 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-from app.irsystem.models.search import handle_query, gen_data
+from app.irsystem.models.search import handle_query, gen_data, clean
+import re
 project_name = "Ilan's Cool Project Template"
 net_id = "Ilan Filonenko: if56"
 
@@ -10,15 +11,35 @@ def search():
 	query = {}
 	if request.method == "POST":
 		default, reviews,_ = gen_data()
-		query['trail'] = None #request.form['trail_name']
-	  	query['keywords'] = {request.form['keywords'].encode('ascii', 'ignore'):1}
+		query['trail'] = None
+		#query['trail'] = request.form['trail_name']
+		if request.form['trail_name'] != '':
+			f = re.compile(request.form['trail_name'])
+			for t in default:
+				if f.search(t['trail_name']):
+					query['trail'] = t
+					print('found')
+					break
+		kwds = {}
+		for word in clean(request.form['keywords'].encode('ascii', 'ignore')):
+			if word not in kwds:
+				kwds[word] = 0
+			kwds[word] += 1
+		if len(kwds) > 0:
+			query['keywords'] = kwds
+		else:
+			query['keywords'] = None
+	  	#query['keywords'] = {request.form['keywords'].encode('ascii', 'ignore'):1}
 	  	#query['distance'] = request.form['distance']
 	  	#query['city'] = request.form['city']
-	  	#query['state'] = request.form['state']
+	  	if request.form['near'] != '':
+	  		query['state'] = request.form['near']
+	  		print(query['state'])
 	  	#query['start_altitude'] = [request.form['start_altitude_lb'], request.form['start_altitude_ub']]
 	  	#print(query)
 	  	ranking = handle_query(query, default, reviews)
 		results = {'r' + str(k):ranking[k]['trail_id'] for k in range(10)}
+		print(ranking[0])
 		return redirect(url_for('irsystem.result',**results))
 	else:
 		default, reviews,_ = gen_data()
